a978b015eb35cc6c8ff2ef14ce1d3733343411a0
Peter Tang <petersktang@gmail.com>
2019-10-14 15:41:27 +0800

    Initial Commit

---
+++ MetalExperimentMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalExperimentMac
+//
+//  Created by Peter Tang on 14/10/2019.
+//  Copyright © 2019 Peter Tang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalExperimentMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalExperimentMac
+//
+//  Created by Peter Tang on 14/10/2019.
+//  Copyright © 2019 Peter Tang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
